d345b5aab02cfed0189d39be7d7cefc37a8822d9
Jean-Marc McGhee <jeanmarc.mcghee@gmail.com>
2022-10-02 14:23:50 +0100

    Add tests for solution 2

--- tests.js
+++ tests.js
@@ -19,7 +19,7 @@
 const cond = (cb, testInputValues) => testInputValues.every(({ inputValue, expectedResult, testName }) => {
   const result = cb(inputValue);
   const passed = cb(inputValue) === expectedResult;
-  console.log(`${testName}: ${passed ? 'PASSED' : 'FAILED'}`, `Expected: ${expectedResult} Result: ${result}`)
+  console.log(`${passed ? '\x1b[32m' : '\x1b[31m'}`, `${testName}: ${passed ? 'PASSED' : 'FAILED'}`, `Expected: ${expectedResult} Result: ${result}`)
   return result === expectedResult;
 })
 
@@ -28,7 +28,7 @@ const cond = (cb, testInputValues) => testInputValues.every(({ inputValue, expec
 */
 
 const testSolution1 = (cb) => {
-    console.log('Question 1: Create a function that can give the integer value of an 8 bit string')
+    console.log('\x1b[33m', 'Question 1: Create a function that can give the integer value of an 8 bit string')
     const tests = [{
         testName: 'Should convert 01010000 correctly',
         inputValue: '01010000',
@@ -58,4 +58,31 @@ const testSolution1 = (cb) => {
     cond(cb, tests);
 }
 
-module.exports = { testSolution1 };
+const testSolution2 = (cb) => {
+    console.log('\x1b[33m', 'Question 2: Create a function that will take an integer within the range of `0-255` and return a 8-bit binary string. Return null if number is out of range')
+    const tests = [{
+        testName: 'Should convert 80 correctly',
+        inputValue: 80,
+        expectedResult: '01010000',
+    },
+    {
+        testName: 'Should convert 255 correctly',
+        inputValue: 255,
+        expectedResult: '11111111',
+    },
+    {
+        testName: 'Should convert 0 correctly',
+        inputValue: 0,
+        expectedResult: '00000000',
+    },
+    {
+        testName: 'Should convert 00000001 correctly',
+        inputValue: 1,
+        expectedResult: '00000001',
+    },
+    ]
+
+    cond(cb, tests);
+}
+
+module.exports = { testSolution1, testSolution2 };
